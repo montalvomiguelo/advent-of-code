87ee8c4a76bf6dbd261b9a4d570b893f7475ba05
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-12-03 13:44:20 -0600

    Day 02 part 2

--- day-02/index.js
+++ day-02/index.js
@@ -2,20 +2,18 @@
  * @param {string[]} lines
  */
 export default function (lines) {
-  /** @type {Record<string, number>} */
-  const config = {
-    red: 12,
-    green: 13,
-    blue: 14
-  }
   let sum = 0
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i]
     if (!line) break
     const parts = line.split(': ')
-    const game = parts[0]
     const sets = parts[1].split('; ')
-    let isPossible = true
+    /** @type {Record<string, number>} */
+    const quantities = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
     for (let j = 0; j < sets.length; j++) {
       const set = sets[j]
       const cubes = set.split(', ')
@@ -23,17 +21,13 @@ export default function (lines) {
         const parts = cubes[k].split(' ')
         const quantity = parseInt(parts[0])
         const color = parts[1]
-        if (quantity > config[color]) {
-          isPossible = false
-          break
+        if (quantity > quantities[color]) {
+          quantities[color] = quantity
         }
       }
-      if (!isPossible) break
     }
-    if (isPossible) {
-      const gameId = parseInt(game.split(' ')[1])
-      sum += gameId
-    }
+    const total = quantities.red * quantities.green * quantities.blue
+    sum += total
   }
   return sum
 }
