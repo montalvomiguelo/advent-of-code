15e5dbe67a3595bd4ab26e23e6fc62435f4a7d48
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-12-02 23:35:26 -0600

    Day 01 part 1

---
+++ day-01/index.js
@@ -0,0 +1,34 @@
+import fs from 'node:fs'
+
+const input = fs.readFileSync('input', 'utf8')
+const lines = input.split('\n')
+let result = 0
+const numbers = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'])
+
+for (let i = 0; i < lines.length; i++) {
+  const line = lines[i]
+  if (!line) break
+  let leftIdx = 0
+  let rightIdx = line.length - 1
+  let leftNum = null
+  let rightNum = null
+  while (leftIdx <= rightIdx) {
+    if (leftNum && rightNum) break
+    if (numbers.has(line[leftIdx]) && !leftNum) {
+      leftNum = line[leftIdx]
+    } else if (!leftNum) {
+      leftIdx++
+    }
+    if (numbers.has(line[rightIdx]) && !rightNum) {
+      rightNum = line[rightIdx]
+    } else if (!rightNum) {
+      rightIdx--
+    }
+  }
+  if (leftNum && !rightNum) rightNum = leftNum
+  if (rightNum && !leftNum) leftNum = rightNum
+  const sum = parseInt(`${leftNum}${rightNum}`)
+  result += sum
+}
+
+console.log(result)
