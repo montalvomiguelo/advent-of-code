564cb95629eade82d9e41c99c93e5665cfe1e25e
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-12-05 14:35:41 -0600

    Day 03 part 2

--- day-03/index.js
+++ day-03/index.js
@@ -2,61 +2,64 @@
  * @param {string[]} lines
  */
 export default function (lines) {
-  let sum = 0
+  /** @type {Map<string, number[]>} */
+  const numbers = new Map()
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i]
     let number = ''
-    let adjacentCount = 0
+    let symbol = ''
     for (let j = 0; j < line.length; j++) {
       const char = line[j]
-      let count = 0
+
       if (/\d/.test(char)) {
         number += char
-        const adjTop = lines[i - 1]?.[j]
-        const adjBottom = lines[i + 1]?.[j]
-        const adjLeft = line[j - 1]
-        const adjRight = line[j + 1]
-        const adjTopLeft = lines[i - 1]?.[j - 1]
-        const adjTopRight = lines[i - 1]?.[j + 1]
-        const adjBottomLeft = lines[i + 1]?.[j - 1]
-        const adjBottomRight = lines[i + 1]?.[j + 1]
         const adjacents = [
-          adjTop,
-          adjBottom,
-          adjLeft,
-          adjRight,
-          adjTopLeft,
-          adjTopRight,
-          adjBottomLeft,
-          adjBottomRight
+          { row: i - 1, col: j, value: lines[i - 1]?.[j] },
+          { row: i + 1, col: j, value: lines[i + 1]?.[j] },
+          { row: i, col: j - 1, value: line[j - 1] },
+          { row: i, col: j + 1, value: line[j + 1] },
+          { row: i - 1, col: j - 1, value: lines[i - 1]?.[j - 1] },
+          { row: i - 1, col: j + 1, value: lines[i - 1]?.[j + 1] },
+          { row: i + 1, col: j - 1, value: lines[i + 1]?.[j - 1] },
+          { row: i + 1, col: j + 1, value: lines[i + 1]?.[j + 1] }
         ]
         for (let k = 0; k < adjacents.length; k++) {
           const adjacent = adjacents[k]
-          if (
-            adjacent !== '.' &&
-            adjacent !== undefined &&
-            !/\d/.test(adjacent)
-          ) {
-            count++
+          const value = adjacent.value
+          if (value === '*') {
+            symbol = `${adjacent.row}:${adjacent.col}}`
           }
         }
-        adjacentCount += count
-
-        if (j === line.length - 1) {
-          if (number && adjacentCount > 0) {
-            sum += parseInt(number)
+      }
+      if (!/\d/.test(char) || j === line.length - 1) {
+        if (number && symbol) {
+          const key = symbol
+          if (numbers.has(key)) {
+            numbers.get(key)?.push(parseInt(number))
+          } else {
+            numbers.set(key, [parseInt(number)])
           }
-          adjacentCount = 0
-          number = ''
         }
-      } else {
-        if (number && adjacentCount > 0) {
-          sum += parseInt(number)
-        }
-        adjacentCount = 0
         number = ''
+        symbol = ''
       }
     }
+  }
+  return sum(numbers)
+}
+
+/**
+ * @param {Map<string, number[]>} numbersMap
+ */
+function sum(numbersMap) {
+  let sum = 0
+  for (const [, numbers] of numbersMap) {
+    if (numbers.length < 2) continue
+    let res = 1
+    for (let i = 0; i < numbers.length; i++) {
+      res *= numbers[i]
+    }
+    sum += res
   }
   return sum
 }
