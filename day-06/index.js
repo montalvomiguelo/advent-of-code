7816255322b062c9fd8a512c21b473e642a96294
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-12-06 23:55:47 -0600

    Day 06 part 2

--- day-06/index.js
+++ day-06/index.js
@@ -2,24 +2,20 @@
  * @param {string[]} lines
  */
 export default function (lines) {
-  const times = lines[0].split(': ')[1].trim().split(' ').filter(Boolean)
-  const distances = lines[1].split(': ')[1].trim().split(' ').filter(Boolean)
-  let result = 1
+  const time = parseInt(
+    lines[0].split(': ')[1].trim().split(' ').filter(Boolean).join('')
+  )
+  const distance = parseInt(
+    lines[1].split(': ')[1].trim().split(' ').filter(Boolean).join('')
+  )
+  let count = 0
 
-  for (let i = 0; i < times.length; i++) {
-    const time = parseInt(times[i])
-    const distance = parseInt(distances[i])
-    let count = 0
-
-    for (let j = 0; j < time; j++) {
-      const traveled = j * (time - j)
-      if (traveled > distance) {
-        count++
-      }
+  for (let j = 0; j < time; j++) {
+    const traveled = j * (time - j)
+    if (traveled > distance) {
+      count++
     }
-
-    result *= count
   }
 
-  return result
+  return count
 }
