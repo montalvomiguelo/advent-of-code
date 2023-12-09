2fb0aa7131158914a0fec1c8b88415b2ce488b77
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-12-09 12:30:31 -0800

    Day 08 part 2

--- day-08/index.js
+++ day-08/index.js
@@ -5,13 +5,12 @@
  */
 export default function (lines) {
   /** @type Graph */
-  const graph = new Map()
+  const adjList = new Map()
   const instructions = lines.splice(0, 1)[0]
-  let end = 'ZZZ'
-  /** @type {string|undefined} */
-  let current = 'AAA'
-  let count = 0
-  let idx = 0
+  /** @type {string[]} */
+  const nodes = []
+  /** @type {number[]} */
+  const counts = []
 
   lines.shift()
 
@@ -22,19 +21,54 @@ export default function (lines) {
     const key = parts[0]
     const left = parts[1].slice(1, 4)
     const right = parts[1].slice(6, 9)
-    graph.set(key, { left, right })
-  }
-
-  while (current && current !== end) {
-    const instruction = instructions[idx]
-    if (instruction === 'L') {
-      current = graph.get(current)?.left
-    } else {
-      current = graph.get(current)?.right
+    adjList.set(key, { left, right })
+    if (key[2] === 'A') {
+      nodes.push(key)
     }
-    idx = idx + 1 < instructions.length ? idx + 1 : 0
-    count++
   }
 
-  return count
+  for (let i = 0; i < nodes.length; i++) {
+    let idx = 0
+    let count = 0
+    let current = nodes[i]
+
+    while (current[2] !== 'Z') {
+      const instruction = instructions[idx]
+
+      if (instruction === 'L') {
+        current = /** @type {{left: string, right: string}} */ (
+          adjList.get(current)
+        ).left
+      } else {
+        current = /** @type {{left: string, right: string}} */ (
+          adjList.get(current)
+        ).right
+      }
+
+      count++
+      idx = idx + 1 < instructions.length ? idx + 1 : 0
+    }
+    counts.push(count)
+  }
+
+  return counts.reduce((acc, curr) => lcm(acc, curr), 1)
+}
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function gcd(a, b) {
+  if (b === 0) return a
+  return gcd(b, a % b)
+}
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function lcm(a, b) {
+  return (a * b) / gcd(a, b)
 }
